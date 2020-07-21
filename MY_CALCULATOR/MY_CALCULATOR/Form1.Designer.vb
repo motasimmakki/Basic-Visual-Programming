39494d2167ee92272fff11f62f8c4798ce14c41d
Motasim <44056349+motasimmakki@users.noreply.github.com>
2020-07-21 10:23:54 +0530

    Complete Calculator desktop application.
    In this application, the user can interact with the GUI and can perform simple mathematical calculations.

---
+++ MY_CALCULATOR/MY_CALCULATOR/Form1.Designer.vb
@@ -0,0 +1,300 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.ForeColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.TextBox1.Location = New System.Drawing.Point(32, 50)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(296, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(32, 112)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(296, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(32, 174)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(296, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkRed
+        Me.Button1.ForeColor = System.Drawing.Color.Yellow
+        Me.Button1.Location = New System.Drawing.Point(32, 217)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(114, 35)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "CLEAR"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.DarkRed
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!)
+        Me.Button2.ForeColor = System.Drawing.Color.Yellow
+        Me.Button2.Location = New System.Drawing.Point(152, 217)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(176, 35)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "X^Y"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.DarkRed
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Button3.ForeColor = System.Drawing.Color.Yellow
+        Me.Button3.Location = New System.Drawing.Point(32, 281)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(114, 35)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "%"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.DarkRed
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Button4.ForeColor = System.Drawing.Color.Yellow
+        Me.Button4.Location = New System.Drawing.Point(152, 281)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(94, 35)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "-"
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.DarkRed
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Button5.ForeColor = System.Drawing.Color.Yellow
+        Me.Button5.Location = New System.Drawing.Point(32, 340)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(114, 36)
+        Me.Button5.TabIndex = 7
+        Me.Button5.Text = "/"
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.DarkRed
+        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!)
+        Me.Button6.ForeColor = System.Drawing.Color.Yellow
+        Me.Button6.Location = New System.Drawing.Point(152, 340)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(94, 36)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "*"
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.DarkRed
+        Me.Button7.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!)
+        Me.Button7.ForeColor = System.Drawing.Color.Yellow
+        Me.Button7.Location = New System.Drawing.Point(255, 281)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(73, 95)
+        Me.Button7.TabIndex = 9
+        Me.Button7.Text = "+"
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(29, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 13)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "FIRST NUMBER :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.ForeColor = System.Drawing.Color.Yellow
+        Me.Label2.Location = New System.Drawing.Point(29, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(108, 13)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "SECOND NUMBER :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Yellow
+        Me.Label3.Location = New System.Drawing.Point(29, 146)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "RESULT :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.ClientSize = New System.Drawing.Size(361, 400)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximumSize = New System.Drawing.Size(377, 439)
+        Me.MinimumSize = New System.Drawing.Size(377, 439)
+        Me.Name = "Form1"
+        Me.Text = "My Calculator"
+        Me.TransparencyKey = System.Drawing.Color.LightGoldenrodYellow
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text = String.Empty Then
+            MessageBox.Show("Please Enter Base Number !")
+        ElseIf TextBox2.Text = String.Empty Then
+            MessageBox.Show("Please Enter Power !")
+        End If
+        Dim i As Integer = 0
+        Dim Result As Double = 1
+        While i < CInt(TextBox2.Text)
+            Result = Result * Convert.ToDouble(TextBox1.Text)
+            i = i + 1
+        End While
+        TextBox3.Text = CStr(Result)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text = String.Empty Then
+            MessageBox.Show("Please Enter First Number !")
+        ElseIf TextBox2.Text = String.Empty Then
+            MessageBox.Show("Please Enter Second Number !")
+        End If
+        Dim Result As Double = 0
+        Result = Convert.ToDouble(TextBox1.Text) Mod Convert.ToDouble(TextBox2.Text)
+        TextBox3.Text = CStr(Result)
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If TextBox1.Text = String.Empty Then
+            MessageBox.Show("Please Enter First Number !")
+        ElseIf TextBox2.Text = String.Empty Then
+            MessageBox.Show("Please Enter Second Number !")
+        End If
+        Dim Result As Double = 0
+        Result = Convert.ToDouble(TextBox1.Text) - Convert.ToDouble(TextBox2.Text)
+        TextBox3.Text = CStr(Result)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If TextBox1.Text = String.Empty Then
+            MessageBox.Show("Please Enter First Number !")
+        ElseIf TextBox2.Text = String.Empty Then
+            MessageBox.Show("Please Enter Second Number !")
+        End If
+        Dim Result As Double = 0
+        Result = Convert.ToDouble(TextBox1.Text) / Convert.ToDouble(TextBox2.Text)
+        TextBox3.Text = CStr(Result)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If TextBox1.Text = String.Empty Then
+            MessageBox.Show("Please Enter First Number !")
+        ElseIf TextBox2.Text = String.Empty Then
+            MessageBox.Show("Please Enter Second Number !")
+        End If
+        Dim Result As Double = 0
+        Result = Convert.ToDouble(TextBox1.Text) * Convert.ToDouble(TextBox2.Text)
+        TextBox3.Text = CStr(Result)
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If TextBox1.Text = String.Empty Then
+            MessageBox.Show("Please Enter First Number !")
+        ElseIf TextBox2.Text = String.Empty Then
+            MessageBox.Show("Please Enter Second Number !")
+        End If
+        Dim Result As Double = 0
+        Result = Convert.ToDouble(TextBox1.Text) + Convert.ToDouble(TextBox2.Text)
+        TextBox3.Text = CStr(Result)
+    End Sub
+End Class
