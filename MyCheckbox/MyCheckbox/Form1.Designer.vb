ecf0df4ec72358db2fb9a181961b811450ed8b0d
Motasim <44056349+motasimmakki@users.noreply.github.com>
2020-07-21 09:47:07 +0530

    Introduction to CheckBox.

---
+++ MyCheckbox/MyCheckbox/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.CheckBox1.Location = New System.Drawing.Point(107, 86)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(111, 29)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Under 15"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(41, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(65, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "AGE :"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.CheckBox2.Location = New System.Drawing.Point(107, 140)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(111, 29)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Under 25"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.CheckBox3.Location = New System.Drawing.Point(107, 193)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(101, 29)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "Over 25"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.DarkRed
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Button1.ForeColor = System.Drawing.Color.Yellow
+        Me.Button1.Location = New System.Drawing.Point(80, 263)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(154, 40)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "SUBMIT"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(307, 343)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximumSize = New System.Drawing.Size(323, 382)
+        Me.MinimumSize = New System.Drawing.Size(323, 382)
+        Me.Name = "Form1"
+        Me.Text = "AGE INFO"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents CheckBox3 As CheckBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ MyCheckbox/MyCheckbox/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If CheckBox1.Checked = False And CheckBox2.Checked = False And CheckBox3.Checked = False Then
+            MessageBox.Show("Please Select Option !")
+        ElseIf CheckBox1.Checked = True And CheckBox2.Checked = True Then
+            MessageBox.Show("Your Age Is Under 15 And Under 25 .")
+        ElseIf CheckBox1.Checked = True Then
+            MessageBox.Show("Your Age Is Under 15 .")
+        ElseIf CheckBox2.Checked = True Then
+            MessageBox.Show("Your Age Is Under 25 .")
+        ElseIf CheckBox3.Checked = True Then
+            MessageBox.Show("Your Age Is Over 25 .")
+        End If
+    End Sub
+End Class
